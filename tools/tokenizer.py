8157a4dabb73a1ef02691e356160afe500c0e0eb
Ilaieri <levinilai972@gmail.com>
2025-06-24 20:58:24 +0100

    update tokenizer to recognise implied multpilication

--- tools/tokenizer.py
+++ tools/tokenizer.py
@@ -1,12 +1,23 @@
 import re
+operators = {"+", "-", "*", "/", "^","(",")"}
 def tokenize(expression):
-    pattern=r'\d+\.?\d*|[a-zA-Z]|\*\*|[+\-*/^()]'
+    pattern=r"\d+\.?\d*|[a-zA-Z]|\*\*|[+\-*/^()]"
     tokens = re.findall(pattern, expression)
-    for i in range(len(tokens)):
-        if tokens[i].isdigit() :
-            tokens[i] = str(int(tokens[i]))
+    # Account for implied multiplication 
+    i=0
+    while i<len(tokens)-1:
+        if tokens[i] not in operators and tokens[i+1] not in operators:
+            tokens.insert(i+1, "*")
+            i += 1
+        elif tokens[i] == ")" and tokens[i+1] not in operators:
+            tokens.insert(i+1, "*")
+            i += 1
+        elif tokens[i] not in operators and tokens[i+1] == "(":
+            tokens.insert(i+1, "*")
+            i += 1
+        i += 1
     return tokens
 # Example usage:
-# print(tokenize("32x+5*(2y-4)"))  # Output: ['32', 'x', '+', '5', '*', '(', '2', 'y', '-', '4', ')']
+print(tokenize("32x+5(2y-4)"))  # Output: ["32", "x", "+", "5", "*", "(", "2", "y", "-", "4", ")"]
 
     
